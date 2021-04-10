695a9aa383e0b9e50c9abe0a775eca3731d606db
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-04-10 14:17:48 +0100

    requette HQL

--- src/main/java/com/sip/ams/repositories/ProviderRepository.java
+++ src/main/java/com/sip/ams/repositories/ProviderRepository.java
@@ -1,5 +1,8 @@
 package com.sip.ams.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,6 @@ import com.sip.ams.entities.Provider;
 @Repository
 public interface ProviderRepository extends CrudRepository<Provider, Long> {
 
+	 @Query("select p from Provider p  where p.address like %?1 and p.name like %?2")
+	    List<Provider> findProviderByAddress(String ad, String name);
 }
